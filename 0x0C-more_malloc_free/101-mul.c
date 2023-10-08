d90bc4af8b0906a385183fb3f7683e2e813dc826
huzzainSaheed <agidingbadis@gmail.com>
2023-10-08 02:37:16 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,7 @@
 #define ERR_MSG "Error"
 /**
  * is_digit - checks if a string contains a non-digit char
- * @s: string evaluation
+ * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
  */
