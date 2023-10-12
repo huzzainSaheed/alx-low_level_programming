8f034c43d7034a86695aff91a5c455f51107a060
huzzainSaheed <agidingbadis@gmail.com>
2023-10-11 21:33:13 -0700

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,36 +1,67 @@
 #include "3-calc.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - return the addition of two numbers
+ * @a: the first number.
+ * @b: the second number.
+ *
+ * Return: the sum of a and b.
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
+/**
+ * op_sub - return the different of two numbers
+ * @a: the first number.
+ * @b: the second number.
+ *
+ * Return: the sum of a and b.
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
+/**
+ * op_mul - return the product of two numbers
+ * @a: the first number.
+ * @b: the second number.
+ *
+ * Return: the product of a and b.
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - return the division of two numbers
+ * @a: the first number.
+ * @b: the second number.
+ *
+ * Return: the quotient of a and b.
+ */
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
+/**
+ * op_mod - return the remaider of the division
+ * @a: the first number
+ * @b: the second numb
+ *
+ * Return: the remiander number.
+ */
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
